ace1ca0b2f48ce7bbb599c63be6f1c12efc0e944
saiprasadgovindu <40757983+saiprasad1586@users.noreply.github.com>
2020-02-12 14:29:17 +0530

    Update pg1.py

--- flask/pg1.py
+++ flask/pg1.py
@@ -1,13 +1,13 @@
 from flask import Flask,render_template
  
 
-app = Flask(__name__, template_folder='/templates')
+app = Flask(__name__, template_folder='\templates')
 
 @app.route("/")
 
 @app.route("/home")
 def home():
-    return render_template('template\home.html')
+    return render_template('home.html')
 
 
 @app.route("/about")
